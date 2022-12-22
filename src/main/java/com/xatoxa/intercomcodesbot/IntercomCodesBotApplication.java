c61fc381ebb49210baa7f3a77557ec57d31a58c0
xatoxa <anton.xatuncev@mail.ru>
2022-12-22 15:52:12 +0300

    project init

---
+++ src/main/java/com/xatoxa/intercomcodesbot/IntercomCodesBotApplication.java
@@ -0,0 +1,13 @@
+package com.xatoxa.intercomcodesbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercomCodesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercomCodesBotApplication.class, args);
+	}
+
+}
