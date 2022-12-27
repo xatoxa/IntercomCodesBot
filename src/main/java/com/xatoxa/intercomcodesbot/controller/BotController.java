b06fd7573e57a657e58cf018d8a6fe0d682be74c
xatoxa <anton.xatuncev@mail.ru>
2022-12-27 16:08:19 +0300

    added a list of commands

--- src/main/java/com/xatoxa/intercomcodesbot/controller/BotController.java
+++ src/main/java/com/xatoxa/intercomcodesbot/controller/BotController.java
@@ -4,18 +4,45 @@ import com.xatoxa.intercomcodesbot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
 public class BotController extends TelegramLongPollingBot {
+    final static String HELP_MESSAGE = """
+            Здесь будет описание всех команд, когда они будут работать""";
+    final static String DEFAULT_MESSAGE = """
+            Ты конечно можешь мне рассказать обо всём на свете, но отвечать я буду только этим сообщением :)""";
 
     final BotConfig config;
 
     public BotController(BotConfig config){
         this.config = config;
+
+        List<BotCommand> commands = new ArrayList<>();
+        commands.add(new BotCommand("/start", "Старт!"));
+        commands.add(new BotCommand("/help", "Как пользоваться этим ботом?"));
+        commands.add(new BotCommand("/add", "Добавить код."));
+        commands.add(new BotCommand("/delete", "Удалить код."));
+        commands.add(new BotCommand("/edit", "Изменить код."));
+        commands.add(new BotCommand("/search", "Найти код."));
+        commands.add(new BotCommand("/all_changes", "Действия всех пользователей."));
+        commands.add(new BotCommand("/my_changes", "Действия текущего пользователя."));
+        commands.add(new BotCommand("/all_codes", "Список всех кодов."));
+
+        try {
+            this.execute(new SetMyCommands(commands, new BotCommandScopeDefault(), null));
+        }catch (TelegramApiException e){
+            log.error(e.getMessage());
+        }
     }
 
     @Override
@@ -34,10 +61,10 @@ public class BotController extends TelegramLongPollingBot {
             String msgText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
-            switch (msgText){
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
+            switch (msgText) {
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/help" -> sendMessage(chatId, HELP_MESSAGE);
+                default -> sendMessage(chatId, DEFAULT_MESSAGE);
             }
         }
     }
